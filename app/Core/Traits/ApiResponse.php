689a5ef7d36016a669c3de988a01a23a2557ce29
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2021-12-25 12:42:21 -0600

    Api response * Clase para manejar las respuestas json

---
+++ app/Core/Traits/ApiResponse.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Core\Traits;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Response;
+
+/**
+ * @author JorgeCoronelG
+ * @version 1.0
+ */
+trait ApiResponse
+{
+    /**
+     * Función que retorna una respuesta JSON exitosa
+     */
+    private function successResponse(ResourceCollection | array $data, int $code): JsonResponse
+    {
+        return response()->json($data, $code);
+    }
+
+    /**
+     * Función que retorna una respuesta JSON erronea
+     */
+    protected function errorResponse(array | string $message, int $code): JsonResponse
+    {
+        return response()->json(['code' => $code, 'error' => $message], $code);
+    }
+
+    /**
+     * Función que retorna una respuesta 204 no content
+     */
+    protected function noContentResponse(): Response
+    {
+        return response()->noContent();
+    }
+
+    /**
+     * Función que retorna un JSON con un listado de registros
+     */
+    protected function showAll(ResourceCollection $collection, int $code = 200): JsonResponse
+    {
+        return $this->successResponse($collection, $code);
+    }
+
+    /**
+     * Función que retorna un JSON con un registro
+     */
+    protected function showOne(JsonResource $resource, int $code = 200): JsonResponse
+    {
+        return $this->successResponse(['data' => $resource], $code);
+    }
+}
