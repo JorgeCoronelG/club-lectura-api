ddf4223f07172f472e6f0f86c9e2f93e7aa04c2f
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2022-01-11 13:36:00 -0600

    Multas * Se crea clase para los campos para multas

--- app/Models/Constants/FineFields.php
+++ app/Models/Constants/FineFields.php
@@ -2,11 +2,12 @@
 
 namespace App\Models\Constants;
 
-class BookLoanFields
+class FineFields
 {
     const COST_TOTAL_DIGITS = 6;
     const COST_TOTAL_DECIMAL = 2;
     const PENDING_STATUS = 'Pendiente';
     const PAID_STATUS = 'Pagado';
     const ALL_STATUS = [self::PENDING_STATUS, self::PAID_STATUS];
+    const FINE_PER_DAY = 5;
 }
