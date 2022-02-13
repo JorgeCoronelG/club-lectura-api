286c02424040503bbf94c0a1ed50006673e3696f
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2022-02-12 21:16:06 -0600

    Core * Se hace refactor de los repositorios y services * Se agregan varios métodos al repository

--- app/Core/Contracts/IBaseRepository.php
+++ app/Core/Contracts/IBaseRepository.php
@@ -8,21 +8,54 @@ namespace App\Core\Contracts;
  */
 interface IBaseRepository
 {
-    public function findAll(): \Illuminate\Database\Eloquent\Collection;
+    public function create(array $data): \Illuminate\Database\Eloquent\Model;
 
-    public function findAllPaginated(array $filters, int $paginated, string $sort = null): \Illuminate\Pagination\LengthAwarePaginator;
+    public function delete(int $id): void;
+
+    public function findAll(array $filter = [], string $sort = null, array $columns = ['*']):
+        \Illuminate\Database\Eloquent\Collection;
+
+    public function findAllPaginated(array $filters, int $limit, string $sort = null, array $columns = ['*']):
+        \Illuminate\Pagination\LengthAwarePaginator;
+
+    public function findByField(string $field, mixed $value, array $columns = ['*']):
+        \Illuminate\Database\Eloquent\Collection;
 
     public function findById(int $id): \Illuminate\Database\Eloquent\Model;
 
-    public function create(array $data): \Illuminate\Database\Eloquent\Model;
-
-    public function update(array $data, \Illuminate\Database\Eloquent\Model $entity): \Illuminate\Database\Eloquent\Model;
-
-    public function delete(\Illuminate\Database\Eloquent\Model $entity): void;
+    public function findByMultipleConditions(array $where, array $columns = ['*']):
+        \Illuminate\Database\Eloquent\Collection;
 
     public function findRandom(): \Illuminate\Database\Eloquent\Model;
 
     public function findRandoms(int $records = 1): \Illuminate\Database\Eloquent\Collection;
 
     public function findRecordsLatest(int $records = 10): \Illuminate\Database\Eloquent\Collection;
+
+    public function findOneWithCondition(string $field, mixed $value, string $operator = '='):
+        \Illuminate\Database\Eloquent\Model;
+
+    public function findWhereIn(string $field, array $values, array $columns = ['*']):
+        \Illuminate\Database\Eloquent\Collection;
+
+    public function findWhereNotIn(string $field, array $values, array $columns = ['*']):
+    \Illuminate\Database\Eloquent\Collection;
+
+    public function has(string $relation): \App\Core\BaseRepository;
+
+    public function hidden(array $fields): \App\Core\BaseRepository;
+
+    public function orderBy(string $column, string $direction = 'ASC'): \App\Core\BaseRepository;
+
+    public function sync(int $id, string $relation, array $attributes, bool $detaching = true): array;
+
+    public function update(int $id, array $data): \Illuminate\Database\Eloquent\Model;
+
+    public function visible(array $fields): \App\Core\BaseRepository;
+
+    public function whereHas(string $relation, \Closure $closure): \App\Core\BaseRepository;
+
+    public function with(array|string $relations): \App\Core\BaseRepository;
+
+    public function withCount(array|string $relations): \App\Core\BaseRepository;
 }
