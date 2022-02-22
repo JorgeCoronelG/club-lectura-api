6529f7943ec27388af9cda58fbc79db7de480161
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2022-02-21 19:35:19 -0600

    Se agrega uso de trait para manejar los paginados y en caso de que no sea paginado, manda la respuesta normal sin los links y meta data

--- app/Http/Resources/Book/BookPortalCollection.php
+++ app/Http/Resources/Book/BookPortalCollection.php
@@ -2,17 +2,29 @@
 
 namespace App\Http\Resources\Book;
 
+use App\Http\Resources\Traits\PaginateCollection;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class BookPortalCollection extends ResourceCollection
 {
+    use PaginateCollection;
+
+    private bool $paginated;
     public $collects = BookPortalResource::class;
+
+    public function __construct(mixed $resource, bool $paginated = false)
+    {
+        parent::__construct($resource);
+        $this->paginated = $paginated;
+    }
 
     /**
      * Transform the resource collection into an array.
      */
-    public function toArray($request)
+    public function toArray($request): array|\JsonSerializable|\Illuminate\Contracts\Support\Arrayable
     {
-        return ['data' => parent::toArray($request)];
+        return ($this->paginated)
+            ? $this->getPaginationCollection($this)
+            : ['data' => parent::toArray($request)];
     }
 }
