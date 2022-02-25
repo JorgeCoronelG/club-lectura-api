dbbdbde9235346f484a4e558b6704002c653872d
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2022-02-25 13:33:15 -0600

    Se crea controlador con función findAll para los géneros literarios

---
+++ app/Http/Controllers/Api/LiteraryGenderController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Contracts\Services\ILiteraryGenderService;
+use App\Core\BaseApiController;
+use App\Http\Resources\LiteraryGender\LiteraryGenderResource;
+use Illuminate\Http\JsonResponse;
+
+class LiteraryGenderController extends BaseApiController
+{
+    protected ILiteraryGenderService $literaryGenderService;
+
+    /**
+     * @param ILiteraryGenderService $literaryGenderService
+     */
+    public function __construct(ILiteraryGenderService $literaryGenderService)
+    {
+        $this->literaryGenderService = $literaryGenderService;
+    }
+
+    public function findAll(): JsonResponse
+    {
+        $literaryGenders = $this->literaryGenderService->findAll();
+        return $this->showAll(LiteraryGenderResource::collection($literaryGenders));
+    }
+}
