5e0c5459602912640ee11c12562ea8b3454ddabc
JorgeCoronelG <tprog.jorge.coronel@outlook.com>
2022-03-16 09:33:44 -0600

    Modelo LiterarySubgender * Se agrega trait Sortable * Se agrega propiedad $allowedSorts * Se implementa interface IScopeFilter

--- app/Models/LiterarySubgender.php
+++ app/Models/LiterarySubgender.php
@@ -2,16 +2,36 @@
 
 namespace App\Models;
 
+use App\Models\Contracts\IScopeFilter;
+use App\Models\Traits\Sortable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class LiterarySubgender extends Model
+class LiterarySubgender extends Model implements IScopeFilter
 {
+    use Sortable;
+
     protected $fillable = [
         'name',
         'literary_gender_id'
     ];
+
+    public array $allowedSorts = ['id', 'name'];
+
+    public function scopeFilter(Builder $query, array $params = []): Builder
+    {
+        if (empty($params)) {
+            return $query;
+        }
+
+        if (isset($params['name']) && trim($params['name']) !== '') {
+            $query->where('name', 'LIKE', "%${params['name']}%");
+        }
+
+        return $query;
+    }
 
     public function literaryGender(): BelongsTo
     {
