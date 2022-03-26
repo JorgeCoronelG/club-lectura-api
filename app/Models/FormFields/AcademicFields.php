92ed94b2fe03b431cba14dfc57ee9ca25e0f860b
JorgeCoronelG <tprog.jorge.coronel@outlook.com>
2022-03-26 17:11:03 -0600

    Se crean AcademicFields.php y StudentFields.php para las validaciones

--- app/Models/FormFields/AcademicFields.php
+++ app/Models/FormFields/AcademicFields.php
@@ -4,5 +4,6 @@ namespace App\Models\FormFields;
 
 class AcademicFields
 {
+    final const REGISTRATION_MIN_LENGTH = 10;
     final const REGISTRATION_MAX_LENGTH = 15;
 }
--- app/Models/FormFields/StudentFields.php
+++ app/Models/FormFields/StudentFields.php
@@ -4,5 +4,6 @@ namespace App\Models\FormFields;
 
 class StudentFields
 {
+    final const GROUP_MIN_LENGTH = 8;
     final const GROUP_MAX_LENGTH = 15;
 }
