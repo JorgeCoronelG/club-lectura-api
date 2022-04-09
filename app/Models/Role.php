4fff8be3ac661f8260fb409edcf790f639fa5480
JorgeCoronelG <tprog.jorge.coronel@outlook.com>
2022-04-09 15:28:42 -0500

    Modelo de rol * Se agrega interfaz y método del scope para filtro * Se agrega propiedad allowedSorts * Se agrega trait sortable

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,18 +2,28 @@
 
 namespace App\Models;
 
+use App\Models\Contracts\IScopeFilter;
+use App\Models\Traits\Sortable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Role extends Model
+class Role extends Model implements IScopeFilter
 {
-    use HasFactory;
+    use HasFactory, Sortable;
 
     protected $fillable = ['name'];
+
+    public array $allowedSorts = ['name'];
 
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class)->withTimestamps();
+    }
+
+    public function scopeFilter(Builder $query, array $params = []): Builder
+    {
+        return $query;
     }
 }
